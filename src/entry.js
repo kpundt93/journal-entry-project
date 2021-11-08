ca56b56c73ad9664780db3cb2dbf3ccf4d1c75fd
mellowmorgan <morgan.waites@gmail.com>
2021-11-08 15:32:48 -0800

    Add methods getTeaser getVowels and getConsonants
    Co-authored-by: Katie Pundt <kzpundt@gmail.com>

--- src/entry.js
+++ src/entry.js
@@ -11,3 +11,41 @@ Entry.prototype.wordCount = function() {
   return total;
 
 };
+
+Entry.prototype.getVowels = function() {
+  const vowels = ["a", "e", "i", "o", "u"];
+  let entry = this.title + this.body;
+  let entryArray = entry.split("");
+  let vowelCount = 0;
+
+  for (let i = 0; i < entryArray.length; i++) {
+    if (vowels.includes(entryArray[i])) {
+      vowelCount++;
+    }
+  } return vowelCount;
+  
+};
+
+Entry.prototype.getConsonants = function() {
+  const consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"];
+  let entry = this.title + this.body;
+  let entryArray = entry.split("");
+  let consonantCount = 0;
+
+  for (let i = 0; i < entryArray.length; i++) {
+    if (consonants.includes(entryArray[i])) {
+      consonantCount++;
+    }
+  } return consonantCount;
+};
+
+Entry.prototype.getTeaser = function(){
+  const len = this.body.length;
+  for(let i=0;i<len;i++){
+    if(this.body[i]==="." || this.body[i]==="!" || this.body[i]==="?"){
+      const firstSentence = this.body.slice(0,i+1);
+      return firstSentence;
+    }
+  }
+  return false;
+};
--- src/index.js
+++ src/index.js
@@ -5,12 +5,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
 
-  $("#formOne").submit(function(event){
-    event.preventDefault();
-    const title = $("#title").val();
-    const body = $("#body").val();
-    let entry = new Entry(title,body);
-    console.log(entry.title + " " + entry.body);
-    console.log("total word count: " + entry.wordCount());
-    
-  });
+$("#formOne").submit(function(event){
+  event.preventDefault();
+  const title = $("#title").val();
+  const body = $("#body").val();
+  let entry = new Entry(title,body);
+  console.log(entry.title + " " + entry.body);
+  console.log("total word count: " + entry.wordCount());
+  console.log("vowel count: " + entry.getVowels());
+  console.log("consonant count: " + entry.getConsonants());
+  console.log("first sentence is: "+ entry.getTeaser());
+});
